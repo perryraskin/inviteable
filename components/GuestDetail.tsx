68300c3a0de6f5486d9c4959f07ec75c25dd0fb5
Perry Raskin <perryraskin@gmail.com>
2021-04-30 19:18:20 -0400

    Updates event detail page

--- components/GuestDetail.tsx
+++ components/GuestDetail.tsx
@@ -6,7 +6,7 @@ import utilities from "../utilities"
 
 interface Props {}
 
-const EventDetail: NextPage<Props> = ({}) => {
+const GuestDetail: NextPage<Props> = ({}) => {
   return (
     <form className="space-y-8 divide-y divide-gray-200">
       <div className="space-y-8 divide-y divide-gray-200">
@@ -23,92 +23,20 @@ const EventDetail: NextPage<Props> = ({}) => {
           <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
             <div className="sm:col-span-4">
               <label
-                htmlFor="title"
+                htmlFor="username"
                 className="block text-sm font-medium text-gray-700"
               >
-                Event title
-              </label>
-              <div className="mt-1">
-                <input
-                  id="title"
-                  name="title"
-                  type="text"
-                  placeholder="Grandma's 90th Birthday Surprise!"
-                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="date"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Date
-              </label>
-              <div className="mt-1">
-                <input
-                  type="date"
-                  name="date"
-                  id="date"
-                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="last_name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Time
-              </label>
-              <div className="mt-1">
-                <input
-                  type="time"
-                  name="last_name"
-                  id="last_name"
-                  autoComplete="family-name"
-                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="location"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Where
-              </label>
-              <div className="mt-1">
-                <input
-                  id="location"
-                  name="location"
-                  type="text"
-                  placeholder="Mom's House"
-                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="price"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Price
+                Username
               </label>
               <div className="mt-1 flex rounded-md shadow-sm">
                 <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
-                  $
+                  workcation.com/
                 </span>
                 <input
-                  type="number"
-                  name="price"
-                  id="price"
-                  value="0.00"
-                  autoComplete="price"
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="username"
                   className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
                 />
               </div>
@@ -119,7 +47,7 @@ const EventDetail: NextPage<Props> = ({}) => {
                 htmlFor="about"
                 className="block text-sm font-medium text-gray-700"
               >
-                Details
+                About
               </label>
               <div className="mt-1">
                 <textarea
@@ -131,9 +59,34 @@ const EventDetail: NextPage<Props> = ({}) => {
                 />
               </div>
               <p className="mt-2 text-sm text-gray-500">
-                Write a few sentences about what you want guests to know about
-                your event.
+                Write a few sentences about yourself.
               </p>
+            </div>
+
+            <div className="sm:col-span-6">
+              <label
+                htmlFor="photo"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Photo
+              </label>
+              <div className="mt-1 flex items-center">
+                <span className="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                  </svg>
+                </span>
+                <button
+                  type="button"
+                  className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Change
+                </button>
+              </div>
             </div>
 
             <div className="sm:col-span-6">
@@ -178,6 +131,163 @@ const EventDetail: NextPage<Props> = ({}) => {
                     PNG, JPG, GIF up to 10MB
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="pt-8">
+          <div>
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Personal Information
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">
+              Use a permanent address where you can receive mail.
+            </p>
+          </div>
+          <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first_name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                First name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  autoComplete="given-name"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last_name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Last name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  autoComplete="family-name"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Country / Region
+              </label>
+              <div className="mt-1">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="sm:col-span-6">
+              <label
+                htmlFor="street_address"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Street address
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="street_address"
+                  id="street_address"
+                  autoComplete="street-address"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="city"
+                className="block text-sm font-medium text-gray-700"
+              >
+                City
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="state"
+                className="block text-sm font-medium text-gray-700"
+              >
+                State / Province
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="state"
+                  id="state"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="zip"
+                className="block text-sm font-medium text-gray-700"
+              >
+                ZIP / Postal
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="zip"
+                  id="zip"
+                  autoComplete="postal-code"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
               </div>
             </div>
           </div>
@@ -342,4 +452,4 @@ const EventDetail: NextPage<Props> = ({}) => {
   )
 }
 
-export default EventDetail
+export default GuestDetail
