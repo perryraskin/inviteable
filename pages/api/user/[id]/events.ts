b5d17d6ca1bbc062fea2b107e37058938f6ab978
Perry Raskin <perryraskin@gmail.com>
2022-04-01 18:01:43 -0400

    Fixes user auth events list

--- pages/api/user/[id]/events.ts
+++ pages/api/user/[id]/events.ts
@@ -4,6 +4,7 @@ import { User } from "@prisma/client"
 import auth from "../../../../middleware/auth"
 import { EventAccess, GuestResponse } from "../../../../models/interfaces"
 import dayjs from "dayjs"
+import e from "express"
 
 export default async function(req: NextApiRequest, res: NextApiResponse) {
   const userAuth = await auth(req, res)
@@ -19,28 +20,33 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
   let events
   if (req.method === "GET") {
     try {
-      events = await prisma.event.findMany({
-        where: {
-          userId: parseInt(userIdString)
-        },
-        include: {
-          Host: true,
-          Address: true,
-          Guests: {
-            include: {
-              User: true
-            }
+      if (parseInt(userIdString) === user.id) {
+        events = await prisma.event.findMany({
+          where: {
+            userId: user.id
           },
-          Settings: true,
-          Invites: true
-        },
-        orderBy: {
-          dateTimeStart: "desc"
-        }
-      })
+          include: {
+            Host: true,
+            Address: true,
+            Guests: {
+              include: {
+                User: true
+              }
+            },
+            Settings: true,
+            Invites: true
+          },
+          orderBy: {
+            dateTimeStart: "asc"
+          }
+        })
 
-      res.status(200)
-      res.json({ authorized: true, events })
+        res.status(200)
+        res.json({ authorized: true, events })
+      } else {
+        res.status(401)
+        res.json({ authorized: false })
+      }
     } catch (err) {
       res.status(500)
       res.json({ authorized: false, error: err.message })
