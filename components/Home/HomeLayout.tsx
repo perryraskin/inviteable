935840d4365987da04f0d8bb66a8aa1b9f38903c
Perry Raskin <perryraskin@gmail.com>
2022-04-03 14:54:13 -0400

    Removes loader from landing pagpe

--- components/Home/HomeLayout.tsx
+++ components/Home/HomeLayout.tsx
@@ -3,39 +3,17 @@ import { NextPage } from "next"
 import Link from "next/link"
 import Router, { useRouter } from "next/router"
 import withLayout from "../../hocs/withLayout"
-import utilities from "../../utilities"
 import dayjs from "dayjs"
 import utc from "dayjs/plugin/utc"
 dayjs.extend(utc)
-import { MagicContext, LoggedInContext, LoadingContext } from "../Store"
 
 import Landing from "./Landing"
 // import Dashboard from "./Dashboard"
-import Section from "../Layout/Section"
-// import Login from "../Forms/Login"
 
 interface Props {}
 
 const HomeLayout: NextPage<Props> = ({}) => {
-  const [isLoading, setIsLoading] = React.useContext(LoadingContext)
-
-  if (isLoading) {
-    return (
-      <Section>
-        <img
-          className="ml-auto mr-auto block text-center rounded-3xl"
-          width="350"
-          src="https://cdn.hackernoon.com/images/0*4Gzjgh9Y7Gu8KEtZ.gif"
-        ></img>
-      </Section>
-    )
-  } else
-    return (
-      <React.Fragment>
-        <Landing />
-        {/* <Login /> */}
-      </React.Fragment>
-    )
+  return <Landing />
 }
 
 export default withLayout(HomeLayout)
